659153b26ef5552b316c508ae2c95d126a987cfb
guerin <fekamguerin@gmail.com>
2024-11-08 11:45:45 -0500

    test

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "app" {
   count = var.instance_count
-  ami = "ami-0c55b159cbfafe1f0" # Exemple d'AMI
+  ami = "ami-0b8c6b923777519db" 
   instance_type = var.instance_type
   subnet_id = element(var.subnet_ids, count.index)
   tags = {
